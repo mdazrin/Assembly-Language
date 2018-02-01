2eaf4fdc5fd8d0b902e20f83080877babd0d02cc
mdazrin <mdazrin.programmer@gmail.com>
2018-02-01 08:33:21 +0800

    Add files via upload

---
+++ Main.asm
@@ -0,0 +1,34 @@
+; Author: Azrin Hadahi
+; Creation date: 1/2/2018
+; Title: My first program
+; Technical details : Simple add of two numbers in register
+
+
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode: DWORD
+
+.data
+ sum DWORD 3	; define your variable here
+
+
+
+.code
+
+
+main PROC
+	; write your assembly code here
+
+	mov eax,3
+	add eax,8
+	add eax,sum
+
+	
+
+
+
+	Invoke ExitProcess, 0
+
+main endp
+end main
